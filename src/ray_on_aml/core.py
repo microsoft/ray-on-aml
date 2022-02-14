d837f3dbe9bd3ce619c28019b76595aeca7329a1
james-tn <--global>
2022-02-14 00:23:46 +0000

    run test cases

--- src/ray_on_aml/core.py
+++ src/ray_on_aml/core.py
@@ -23,7 +23,7 @@ __version__='0.0.7'
 
 class Ray_On_AML():
     def __init__(self,compute_cluster=None, ws=None, base_conda_dep =['adlfs==2021.10.0','pip==21.3.1'], 
-    base_pip_dep = ['ray[tune]==1.9.2', 'ray[rllib]==1.9.2','ray[serve]==1.9.2', 'xgboost_ray==0.1.6', 'dask==2021.12.0','pyarrow >= 5.0.0','fsspec==2021.10.1','fastparquet==0.7.2','tabulate==0.8.9','raydp'], 
+    base_pip_dep = ['ray[tune]==1.10.0', 'ray[rllib]==1.10.0','ray[serve]==1.10.0', 'xgboost_ray==0.1.6', 'dask==2021.12.0','pyarrow >= 5.0.0','fsspec==2021.10.1','fastparquet==0.7.2','tabulate==0.8.9','raydp'], 
     vnet_rg = None, vm_size=None, vnet=None, subnet=None,
     exp_name ='ray_on_aml', maxnode =5, additional_conda_packages=[],additional_pip_packages=[], job_timeout=600000):
         """ Class for Ray_On_AML
